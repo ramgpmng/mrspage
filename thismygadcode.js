bd8e42585ebc16629d68f6403f8e5bdab8146164
ramgpmng <rishe3012@gmail.com>
2016-06-14 17:37:04 +0000

    Add files via upload

---
+++ thismygadcode.js
@@ -0,0 +1,77 @@
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js">
+</script><style type="text/css">
+#fanback{
+display:none;
+background:rgba(0,0,0,0.9);
+width:100%;
+height:100%;
+position:fixed;
+top:0;
+left:0;
+z-index:99999}
+#fan-exit{
+width:100%;
+height:100%
+}
+#fanbox{
+background:none;
+width:100%;
+height:auto;
+position:absolute;
+top:30%;
+}
+
+</style><script type="text/javascript">/*<![CDATA[
+jQuery.cookie=function(key,value,options){
+if(arguments.length>1&&String(value)!=="[object Object]"){
+options=jQuery.extend({},options);
+if(value===null||value===undefined){
+options.expires=-1;
+}
+if(typeof options.expires==='number'){
+var days=options.expires,t=options.expires=new Date();
+t.setDate(t.getDate()+days);
+}
+value=String(value);
+return(document.cookie=[
+encodeURIComponent(key),'=',
+options.raw?value:encodeURIComponent(value),
+options.expires?'; expires='+options.expires.toUTCString():'',options.path?'; path='+options.path:'',
+options.domain?'; domain='+options.domain:'',
+options.secure?'; secure':''
+].join(''));
+}
+
+options=value||{};
+var result,decode=options.raw?function(s){return s;}:decodeURIComponent;
+return(result=new RegExp('(?:^|; )'+encodeURIComponent(key)+'=([^;]*)').exec(document.cookie))?decode(result[1]):null;
+};
+/*]]>*/</script><script type="text/javascript">jQuery(document).ready(function($){
+if(document.cookie.indexOf('visited=true')==-1){var fifteenDays=1000*60*60*6;var expires=new Date((new Date()).valueOf()+fifteenDays);document.cookie="visited=true;expires="+expires.toUTCString();
+$('#fanback').delay(8000).fadeIn('medium');
+$('#fanclose, #fan-exit')(function(){
+$('#fanback').stop().fadeOut('medium');
+});}});
+</script><br /><div id="fanback"><div id="fan-exit"></div><div id="fanbox"><div class="remove-borda"></div>
+<center>
+<div class="asd">
+<div class="cds"></div>
+<div class="bds"></div>
+<script type="text/javascript">
+    google_ad_client = "ca-pub-7657970807660310";
+    google_ad_slot = "6019600185";
+    google_ad_width = 336;
+    google_ad_height = 280;
+    google_language = 'en';
+</script>
+<!-- sidebaradunit -->
+<script type="text/javascript"
+src="//pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+</div>
+
+</center>
+</div>
+</div>
+</div>
+<div class='clear'></div>
